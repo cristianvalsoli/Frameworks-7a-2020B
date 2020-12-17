ab4687264820ace88b2e50c4a6a2e607515cd905
System21 <64744539+cristianvalsoli@users.noreply.github.com>
2020-12-17 14:21:39 -0500

    trainninApp

---
+++ mi_primera_pagina/market/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(stock)
+admin.site.register(Vendor)
+admin.site.register(products)
+admin.site.register(Client)
